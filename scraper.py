253d67599f5a8ed35250d8def58e2147014400b1
Pk_Patel <parthpatel9414@gmail.com>
2023-07-25 18:13:20 -0400

    - initial modification to scarper file

--- scraper.py
+++ scraper.py
@@ -1,41 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
+import time
+
 def get_page_content(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        raise Exception(f"Failed to fetch the page. Status code: {response.status_code}")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+    retries = 3
+    for i in range(retries):
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.content
+        elif response.status_code == 503:
+            print(f"Received 503 status code. Retrying in 5 seconds... ({i+1}/{retries})")
+            time.sleep(10)
+        else:
+            raise Exception(f"Failed to fetch the page. Status code: {response.status_code}")
+    raise Exception(f"Failed to fetch the page after {retries} retries.")
 
 def scrape_data(url):
     page_content = get_page_content(url)
     soup = BeautifulSoup(page_content, 'html.parser')
+    print(extract_question_and_answers(soup))
 
-    # Find the relevant elements and extract the data
-    # Modify these selectors according to the website's structure
-    exam_title = soup.select_one('h1[class="page-header"]').text.strip()
-    exam_code = soup.select_one('div[class="text-muted mb-3"]').text.strip()
-    
-    # Assuming the content is inside a table with class "table"
-    table = soup.find('table', class_='table')
-    data = []
-    if table:
-        rows = table.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            if len(cols) >= 2:
-                key = cols[0].text.strip()
-                value = cols[1].text.strip()
-                data.append((key, value))
-
-    return exam_title, exam_code, data
+def extract_question_and_answers(soup):
+    question_card = soup.findAll('div', class_='card exam-question-card')[0]
+    question_number = question_card.find('div', class_='card-header').text.strip().split()[1]
+    question_topic = question_card.find('span', class_='question-title-topic').text.strip()
+    question_text = question_card.find('div', class_='card-body').p.text.strip()
+    answer_choices = {}
+    for choice in question_card.find_all('li', class_='multi-choice-item'):
+        answer_letter = choice.find('span', class_='multi-choice-letter').text.strip().replace('.', '')
+        answer_text = choice.text.strip().strip().replace('\n', ' ').replace('\r', '').strip().replace('  ', '').replace(answer_letter, '').replace('. ','')
+        answer_choices[answer_letter]= answer_text
+    return question_number, question_topic, question_text, answer_choices
 
 if __name__ == "__main__":
     url = "https://www.examtopics.com/exams/google/professional-data-engineer/view/"
-    exam_title, exam_code, data = scrape_data(url)
-    print(f"Exam Title: {exam_title}")
-    print(f"Exam Code: {exam_code}")
-    print("Exam Details:")
-    for key, value in data:
-        print(f"{key}: {value}")
+    scrape_data(url)
+
