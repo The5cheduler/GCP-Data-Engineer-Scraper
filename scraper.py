002b4cfb4d696ea2f49c5a30425f53a00e2f7244
Pk_Patel <parthpatel9414@gmail.com>
2023-07-25 18:22:13 -0400

    - scanning thorough all questions on the page

--- scraper.py
+++ scraper.py
@@ -25,16 +25,17 @@ def scrape_data(url):
     print(extract_question_and_answers(soup))
 
 def extract_question_and_answers(soup):
-    question_card = soup.findAll('div', class_='card exam-question-card')[0]
-    question_number = question_card.find('div', class_='card-header').text.strip().split()[1]
-    question_topic = question_card.find('span', class_='question-title-topic').text.strip()
-    question_text = question_card.find('div', class_='card-body').p.text.strip()
-    answer_choices = {}
-    for choice in question_card.find_all('li', class_='multi-choice-item'):
-        answer_letter = choice.find('span', class_='multi-choice-letter').text.strip().replace('.', '')
-        answer_text = choice.text.strip().strip().replace('\n', ' ').replace('\r', '').strip().replace('  ', '').replace(answer_letter, '').replace('. ','')
-        answer_choices[answer_letter]= answer_text
-    return question_number, question_topic, question_text, answer_choices
+    question_card = soup.findAll('div', class_='card exam-question-card')
+    for question in question_card:
+        question_number = question.find('div', class_='card-header').text.strip().split()[1]
+        question_topic = question.find('span', class_='question-title-topic').text.strip()
+        question_text = question.find('div', class_='card-body').p.text.strip()
+        answer_choices = {}
+        for choice in question.find_all('li', class_='multi-choice-item'):
+            answer_letter = choice.find('span', class_='multi-choice-letter').text.strip().replace('.', '')
+            answer_text = choice.text.strip().strip().replace('\n', ' ').replace('\r', '').strip().replace('  ', '').replace(answer_letter, '').replace('. ','')
+            answer_choices[answer_letter]= answer_text
+        return question_number, question_topic, question_text, answer_choices
 
 if __name__ == "__main__":
     url = "https://www.examtopics.com/exams/google/professional-data-engineer/view/"
